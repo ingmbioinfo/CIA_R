3f65668c69ca7d8fb84c120309e0671b62811307
Federico Marini <marinif@uni-mainz.de>
2024-03-24 14:30:44 +0100

    splitting main functions f(scope)

--- R/CIA_core.R
+++ R/CIA_core.R
@@ -27,9 +27,9 @@ load_signatures <- function(signatures_input) {
 
 #' Compute Signature Scores
 #'
-#' Calculates signature scores for a given set of genes by comparing their expression levels 
-#' in the provided data. The score for each gene set is calculated as the product of the 
-#' count of genes expressed (presence) in a given cell and their relative expression levels (expression), 
+#' Calculates signature scores for a given set of genes by comparing their expression levels
+#' in the provided data. The score for each gene set is calculated as the product of the
+#' count of genes expressed (presence) in a given cell and their relative expression levels (expression),
 #' normalized by the total expression of the cell.
 #'
 #' @param data Numeric matrix, data frame, SeuratObject, or SingleCellExperiment with genes in rows and cells in columns.
@@ -56,19 +56,19 @@ compute_signature_scores <- function(data, geneset, seurat_assay="RNA", matrix="
   if (is.null(total_col_sums)) {
     total_col_sums <- colSums2(datam)
   }
-  
+
   subdata <- datam[geneset, , drop = FALSE]
   count <- colSums2(subdata > 0)
   exp <- colSums2(subdata) / total_col_sums
-  
+
   return(count * exp)
 }
 
 
 ## ' Calculate Signature Scores
 #'
-#' This function calculates the signature scores for each gene set in the provided data. 
-#' It loads the gene signatures from an input source and computes the scores based on 
+#' This function calculates the signature scores for each gene set in the provided data.
+#' It loads the gene signatures from an input source and computes the scores based on
 #' the expression levels of these genes in the data.
 #'
 #' @param data A numeric matrix, data frame, SeuratObject, or SingleCellExperiment with genes in rows and cells in columns,
@@ -84,7 +84,7 @@ compute_signature_scores <- function(data, geneset, seurat_assay="RNA", matrix="
 #' @return Matrix of signature scores if return_score=TRUE or if data is a matrix/data,frame.Otherwise, updates the input object's metadata with scores.
 #' @importFrom future future_lapply plan
 #' @importFrom data.table fread
-#' @importFrom sparseMatrixStats colSums2 
+#' @importFrom sparseMatrixStats colSums2
 #' @export
 
 signature_score <- function(data, signatures_input, return_score=FALSE, seurat_assay="RNA",matrix="data", score_mode = 'raw', n_cpus = NULL) {
@@ -96,7 +96,7 @@ signature_score <- function(data, signatures_input, return_score=FALSE, seurat_a
     if(matrix=='data'){matrix<-'logcounts'}
     datam <- assay(data, matrix)
   } else {datam <- data}
-    
+
   cat('Checking if genes are in the dataset matrix...', "\n")
   result <- sapply(names(signatures), function(x) {
     lt <- length(signatures[[x]])
@@ -109,7 +109,7 @@ signature_score <- function(data, signatures_input, return_score=FALSE, seurat_a
   n_cpus <- if (is.null(n_cpus)) { ceiling(availableCores() / 4) } else { n_cpus }
   tot <- colSums2(datam)
   plan(multicore, workers = n_cpus)
-  
+
   scores <- future_lapply(names(signatures), future.seed=TRUE, FUN =function(name) {
     geneset <- signatures[[name]]
     compute_signature_scores(datam, geneset, tot)
@@ -117,7 +117,7 @@ signature_score <- function(data, signatures_input, return_score=FALSE, seurat_a
 
   scores_df <- do.call(cbind, scores)
   colnames(scores_df) <- names(signatures)
-  
+
   if (score_mode == 'scaled') {
     scores_df <- sweep(scores_df, 2, apply(scores_df, 2, max), `/`)
   } else if (score_mode %in% c('log', 'log2', 'log10')) {
@@ -125,23 +125,23 @@ signature_score <- function(data, signatures_input, return_score=FALSE, seurat_a
   } else if (score_mode != 'raw') {
     stop("Invalid score_mode. Must be one of: ['raw', 'scaled', 'log', 'log2', 'log10']")
   }
-    
-    
+
+
   if (return_score){
-    return(scores_df)    
+    return(scores_df)
   }
-    
+
 if (inherits(data, "Seurat")) {
     data@meta.data[, colnames(scores_df)] <- scores_df
     cat('Scores have been added in data@meta.data', "\n")
     return(data)
-    
+
   } else if (inherits(data, "SingleCellExperiment")) {
     colData(data)[, colnames(scores_df)] <- scores_df
     cat('Scores have been added in colData(data)', "\n")
     return(data)
   } else {return(scores_df)}
-  
+
 }
 
 
@@ -170,7 +170,7 @@ if (inherits(data, "Seurat")) {
 #' @export
 
 signature_based_classification <- function(data, signatures_input, n_cpus = NULL, similarity_threshold = 0.1,
-                                           seurat_assay="RNA",matrix="data",column_name='CIA_prediction', 
+                                           seurat_assay="RNA",matrix="data",column_name='CIA_prediction',
                                            unassigned_label='Unassigned') {
   start_time <- Sys.time()  # Capture start time
 
@@ -183,21 +183,21 @@ signature_based_classification <- function(data, signatures_input, n_cpus = NULL
     }
   }
 
-  score_matrix <- signature_score(data, signatures_input, score_mode = 'scaled',seurat_assay=seurat_assay, 
+  score_matrix <- signature_score(data, signatures_input, score_mode = 'scaled',seurat_assay=seurat_assay,
                                   matrix=matrix, return_score=T, n_cpus = n_cpus)
   labels <- apply(score_matrix, 1, get_label)
 
   end_time <- Sys.time()  # Capture end time
 
   # Format and print the message correctly
-  cat("\nClassification complete!    Start:", format(start_time, "%H:%M:%S"), 
+  cat("\nClassification complete!    Start:", format(start_time, "%H:%M:%S"),
       "    End:", format(end_time, "%H:%M:%S"), "\n")
- 
+
 if (inherits(data, "Seurat")) {
     data@meta.data[,column_name] <-as.factor(labels)
     cat(column_name,'has been added in data@meta.data', "\n")
     return(data)
-  } else if (inherits(data, "SingleCellExperiment")) { 
+  } else if (inherits(data, "SingleCellExperiment")) {
     colData(data)[,column_name]<- as.data.frame(labels)
     cat(column_name,'has been added in colData(data)', "\n")
     return(data)
@@ -205,117 +205,3 @@ if (inherits(data, "Seurat")) {
   else {return(as.factor(labels))}
 }
 
-
-#' Compute Classification Metrics
-#'
-#' @description
-#' Computes the main metrics of classification by comparing the labels of cells classified
-#' by given methods to labels assigned by a reference method. Cells labeled as `unassigned_label`
-#' are excluded from the metrics calculation. Additionally, the percentage of unassigned cells
-#' for each classification method is reported.
-#'
-#' @param data A data frame or matrix representing the cell data, typically an AnnData object in R.
-#' @param classification_col A vector of strings specifying the column names in `data` where the
-#' labels assigned by the methods of interest are stored.
-#' @param groups_col A string specifying the column name in `data` where the labels assigned by
-#' the reference method are stored.
-#' @param unassigned_label A string representing the label used to mark unassigned cells in the
-#' classification columns. Default is an empty string, which means no cells are excluded based on
-#' their label.
-#'
-#' @return A data frame containing the overall sensitivity (SE), specificity (SP), precision (PR),
-#' accuracy (ACC), F1-score (F1), and percentage of unassigned cells (%UN) for each classification
-#' method compared to the reference method.
-#'
-#' @export
-classification_metrics <- function(data, classification_col, groups_col, unassigned_label = '') {
-  report <- purrr::map_dfr(classification_col, function(m) {
-    total_cells <- nrow(data)
-    unassigned_count <- sum(data[[m]] == unassigned_label)
-    percent_unassigned <- (unassigned_count / total_cells) * 100
-
-    filtered_data <- data[data[[m]] != unassigned_label, ]
-
-    metrics <- lapply(unique(filtered_data[[groups_col]]), function(group) {
-      TP <- sum(filtered_data[[m]] == group & filtered_data[[groups_col]] == group)
-      TN <- sum(filtered_data[[m]] != group & filtered_data[[groups_col]] != group)
-      FP <- sum(filtered_data[[m]] == group & filtered_data[[groups_col]] != group)
-      FN <- sum(filtered_data[[m]] != group & filtered_data[[groups_col]] == group)
-
-      SE <- TP / (TP + FN)
-      SP <- TN / (TN + FP)
-      PR <- TP / (TP + FP)
-      ACC <- (TP + TN) / (TP + TN + FP + FN)
-      F1 <- 2 * TP / (2 * TP + FP + FN)
-
-      c(SE = SE, SP = SP, PR = PR, ACC = ACC, F1 = F1)
-    }) %>% dplyr::bind_rows(.f = mean)
-
-    c(metrics, `%UN` = percent_unassigned)
-  }, .id = "Method")
-
-  rownames(report) <- classification_col
-  return(report)
-}
-
-#' Compute Classification Metrics
-#'
-#' Calculates classification metrics such as sensitivity (SE), specificity (SP),
-#' precision (PR), accuracy (ACC), F1-score (F1), and the percentage of 'Unassigned' cells (%UN)
-#' for given classification results, based on the specified unassigned label.
-#'
-#' @param data Data frame containing the observed data including classification and true group labels.
-#' @param classification_col Character vector of column names in `data` that contain the classification results to evaluate.
-#' @param groups_cols Character string specifying the column in `data` that contains the true group labels.
-#' @param unassigned_label The label used in `classification_col` to denote unclassified or unassigned samples. 
-#' If this label is present, %UN will be calculated and included in the metrics.
-#'
-#' @return A matrix with classification metrics (SE, SP, PR, ACC, F1) for each classifier.
-#' If unassigned_label is specified and present in the data, an additional '%UN' column is included in the output.
-#'
-#' @examples
-#' # Assuming data is a data frame with true labels and classification results
-#' data <- read.csv('your_data_file.csv')
-#' classification_col <- c('classifier1', 'classifier2')
-#' groups_cols <- 'true_group'
-#' unassigned_label <- 'Unassigned' # Specify the label that denotes unassigned samples
-#' metrics_report <- compute_metrics(data, classification_col, groups_cols, unassigned_label)
-#' print(metrics_report)
-#'
-#' @export
-compute_metrics <- function(data, classification_col, groups_cols, unassigned_label = '') {
-  report <- list()
-
-  for (m in classification_col) {
-    TP_l <- TN_l <- FP_l <- FN_l <- numeric()
-    UN <- round(100 * sum(data[[m]] == unassigned_label) / nrow(data), 2)
-    datam <- data[data[,m] != unassigned_label,]
-
-    for (i in unique(datam[[groups_cols]])) {
-      TP_l <- c(TP_l, sum(datam[[m]] == i & datam[[groups_cols]] == i))
-      TN_l <- c(TN_l, sum(datam[[m]] != i & datam[[groups_cols]] != i))
-      FP_l <- c(FP_l, sum(datam[[m]] == i & datam[[groups_cols]] != i))
-      FN_l <- c(FN_l, sum(datam[[m]] != i & datam[[groups_cols]] == i))
-    }
-
-    TP <- sum(TP_l)
-    TN <- sum(TN_l)
-    FP <- sum(FP_l)
-    FN <- sum(FN_l)
-
-    SE <- TP / (TP + FN)
-    SP <- TN / (TN + FP)
-    PR <- TP / (TP + FP)
-    ACC <- (TP + TN) / (TP + TN + FP + FN)
-    F1 <- 2 * TP / (2 * TP + FP + FN)
-
-    report[[m]] <- c(SE, SP, PR, ACC, F1, UN)
-  }
-
-  report <- t(as.data.frame(report))
-  colnames(report) <- c('SE', 'SP', 'PR', 'ACC', 'F1', '%UN')
-  if (sum(report[,'%UN'])==0){
-      report <- report[,1:5]
-  }
-  return(report)
-}
---
+++ R/CIA_metrics.R
@@ -0,0 +1,114 @@
+
+#' Compute Classification Metrics
+#'
+#' @description
+#' Computes the main metrics of classification by comparing the labels of cells classified
+#' by given methods to labels assigned by a reference method. Cells labeled as `unassigned_label`
+#' are excluded from the metrics calculation. Additionally, the percentage of unassigned cells
+#' for each classification method is reported.
+#'
+#' @param data A data frame or matrix representing the cell data, typically an AnnData object in R.
+#' @param classification_col A vector of strings specifying the column names in `data` where the
+#' labels assigned by the methods of interest are stored.
+#' @param groups_col A string specifying the column name in `data` where the labels assigned by
+#' the reference method are stored.
+#' @param unassigned_label A string representing the label used to mark unassigned cells in the
+#' classification columns. Default is an empty string, which means no cells are excluded based on
+#' their label.
+#'
+#' @return A data frame containing the overall sensitivity (SE), specificity (SP), precision (PR),
+#' accuracy (ACC), F1-score (F1), and percentage of unassigned cells (%UN) for each classification
+#' method compared to the reference method.
+#'
+#' @export
+classification_metrics <- function(data, classification_col, groups_col, unassigned_label = '') {
+  report <- purrr::map_dfr(classification_col, function(m) {
+    total_cells <- nrow(data)
+    unassigned_count <- sum(data[[m]] == unassigned_label)
+    percent_unassigned <- (unassigned_count / total_cells) * 100
+
+    filtered_data <- data[data[[m]] != unassigned_label, ]
+
+    metrics <- lapply(unique(filtered_data[[groups_col]]), function(group) {
+      TP <- sum(filtered_data[[m]] == group & filtered_data[[groups_col]] == group)
+      TN <- sum(filtered_data[[m]] != group & filtered_data[[groups_col]] != group)
+      FP <- sum(filtered_data[[m]] == group & filtered_data[[groups_col]] != group)
+      FN <- sum(filtered_data[[m]] != group & filtered_data[[groups_col]] == group)
+
+      SE <- TP / (TP + FN)
+      SP <- TN / (TN + FP)
+      PR <- TP / (TP + FP)
+      ACC <- (TP + TN) / (TP + TN + FP + FN)
+      F1 <- 2 * TP / (2 * TP + FP + FN)
+
+      c(SE = SE, SP = SP, PR = PR, ACC = ACC, F1 = F1)
+    }) %>% dplyr::bind_rows(.f = mean)
+
+    c(metrics, `%UN` = percent_unassigned)
+  }, .id = "Method")
+
+  rownames(report) <- classification_col
+  return(report)
+}
+
+#' Compute Classification Metrics
+#'
+#' Calculates classification metrics such as sensitivity (SE), specificity (SP),
+#' precision (PR), accuracy (ACC), F1-score (F1), and the percentage of 'Unassigned' cells (%UN)
+#' for given classification results, based on the specified unassigned label.
+#'
+#' @param data Data frame containing the observed data including classification and true group labels.
+#' @param classification_col Character vector of column names in `data` that contain the classification results to evaluate.
+#' @param groups_cols Character string specifying the column in `data` that contains the true group labels.
+#' @param unassigned_label The label used in `classification_col` to denote unclassified or unassigned samples.
+#' If this label is present, %UN will be calculated and included in the metrics.
+#'
+#' @return A matrix with classification metrics (SE, SP, PR, ACC, F1) for each classifier.
+#' If unassigned_label is specified and present in the data, an additional '%UN' column is included in the output.
+#'
+#' @examples
+#' # Assuming data is a data frame with true labels and classification results
+#' data <- read.csv('your_data_file.csv')
+#' classification_col <- c('classifier1', 'classifier2')
+#' groups_cols <- 'true_group'
+#' unassigned_label <- 'Unassigned' # Specify the label that denotes unassigned samples
+#' metrics_report <- compute_metrics(data, classification_col, groups_cols, unassigned_label)
+#' print(metrics_report)
+#'
+#' @export
+compute_metrics <- function(data, classification_col, groups_cols, unassigned_label = '') {
+  report <- list()
+
+  for (m in classification_col) {
+    TP_l <- TN_l <- FP_l <- FN_l <- numeric()
+    UN <- round(100 * sum(data[[m]] == unassigned_label) / nrow(data), 2)
+    datam <- data[data[,m] != unassigned_label,]
+
+    for (i in unique(datam[[groups_cols]])) {
+      TP_l <- c(TP_l, sum(datam[[m]] == i & datam[[groups_cols]] == i))
+      TN_l <- c(TN_l, sum(datam[[m]] != i & datam[[groups_cols]] != i))
+      FP_l <- c(FP_l, sum(datam[[m]] == i & datam[[groups_cols]] != i))
+      FN_l <- c(FN_l, sum(datam[[m]] != i & datam[[groups_cols]] == i))
+    }
+
+    TP <- sum(TP_l)
+    TN <- sum(TN_l)
+    FP <- sum(FP_l)
+    FN <- sum(FN_l)
+
+    SE <- TP / (TP + FN)
+    SP <- TN / (TN + FP)
+    PR <- TP / (TP + FP)
+    ACC <- (TP + TN) / (TP + TN + FP + FN)
+    F1 <- 2 * TP / (2 * TP + FP + FN)
+
+    report[[m]] <- c(SE, SP, PR, ACC, F1, UN)
+  }
+
+  report <- t(as.data.frame(report))
+  colnames(report) <- c('SE', 'SP', 'PR', 'ACC', 'F1', '%UN')
+  if (sum(report[,'%UN'])==0){
+    report <- report[,1:5]
+  }
+  return(report)
+}
