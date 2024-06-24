cb0ac0896672a66091e59732a2fc4057910ec2b1
Ivan Ferrari <72440462+ivanferrari95@users.noreply.github.com>
2024-06-24 21:10:39 +0200

    Update CIA_extra.R

--- R/CIA_extra.R
+++ R/CIA_extra.R
@@ -1,49 +1,37 @@
-# TO BE TESTED
-# TO BE TESTED
-# TO BE TESTED
-#' Celltypist Majority Vote
+#' Perform Majority Voting on Cell Type Classifications
 #'
+#' A function that wraps Celltypist majority voting (DOI: 10.1126/science.abl5197). 
 #' Assigns cell group labels based on the majority voting of cell type predictions within each group.
 #' If no reference cell groups are provided, an over-clustering step is performed using the Leiden algorithm.
 #'
-#' @param data A `Seurat` or `SingleCellExperiment` object containing the cell data.
-#' @param classification_obs A string or list of strings specifying the column(s) in metadata where the cell type predictions (labels) are stored.
-#' @param groups_obs A string specifying the column in metadata where the reference group labels are stored. If NULL, an over-clustering with the Leiden algorithm is performed based on the dataset size.
-#' @param graph A string specifying the graph to use for clustering in `Seurat` objects. Default is "RNA_snn".
-#' @param min_prop A numeric value specifying the minimum proportion of cells required to assign a majority vote label to a group. Default is 0.
-#' @param unassigned_label A string specifying the label to assign to cell groups where no cell type reaches the minimum proportion. Default is 'Unassigned'.
-#'
-#' @return The modified `Seurat` or `SingleCellExperiment` object with new majority voting classification labels stored in metadata.
+#' @param data A Seurat or SingleCellExperiment object containing the single-cell data.
+#' @param classification_obs A character vector specifying the column names in the metadata that contain cell type classifications.
+#' @param groups_obs A character string specifying the column name in the metadata that contains the grouping information. If NULL, over-clustering will be performed.
+#' @param graph A character string specifying the graph name used for clustering in Seurat. Default is "RNA_snn".
+#' @param min_prop A numeric value specifying the minimum proportion threshold for assigning a majority vote label. Default is 0.
+#' @param unassigned_label A character string specifying the label for groups that do not meet the minimum proportion threshold. Default is 'Unassigned'.
+#' @param res A numeric value specifying the resolution parameter for the Leiden clustering algorithm. Default is 0.05.
+#' 
+#' @return A Seurat or SingleCellExperiment object with updated metadata containing the majority vote labels.
 #'
 #' @details
-#' This function computes the majority voting for cell type labels within each group of cells.
-#' If `groups_obs` is not provided, the function performs over-clustering using the Leiden algorithm with a resolution adjusted based on the dataset size.
-#' The results of the majority voting are stored back in the metadata of the object, adding a column for each classification considered.
-#'
-#' @importFrom clusterExperiment ClusterExperiment
-#' @importFrom Seurat FindClusters
-#' @importFrom S4Vectors DataFrame
-#'
-#' @export
+#' The function can handle both Seurat and SingleCellExperiment objects. If the grouping information (`groups_obs`) is not provided, the function will perform over-clustering using the Leiden algorithm on the specified graph. The majority vote label for each group is determined by the most frequent cell type label within the group. Groups that do not meet the `min_prop` threshold are assigned the `unassigned_label`.
 #'
 #' @examples
 #' \dontrun{
 #' # Example usage with Seurat object
-#' seurat_obj <- celltypist_majority_vote(
-#'   seurat_obj,
-#'   classification_obs = "predicted_labels",
-#'   graph = "RNA_snn",
-#'   min_prop = 0.5
-#' )
+#' seurat_obj <- celltypist_majority_vote(seurat_obj, classification_obs = "predicted_labels", graph = "RNA_snn", min_prop = 0.5)
 #'
 #' # Example usage with SingleCellExperiment object
-#' sce_obj <- celltypist_majority_vote(
-#'   sce_obj,
-#'   classification_obs = "predicted_labels",
-#'   min_prop = 0.5
-#' )
+#' sce_obj <- celltypist_majority_vote(sce_obj, classification_obs = "predicted_labels", min_prop = 0.5)
 #' }
-celltypist_majority_vote <- function(data, classification_obs, groups_obs = NULL, graph = "RNA_snn", min_prop = 0, unassigned_label = 'Unassigned') {
+#'
+#' @import Seurat
+#' @import SingleCellExperiment
+#' @import igraph
+#' @importFrom S4Vectors DataFrame
+#' @export
+celltypist_majority_vote <- function(data, classification_obs, groups_obs = NULL, graph = "RNA_snn", min_prop = 0, unassigned_label = 'Unassigned', res=0.05) {
   if (!is(data, "Seurat") && !is(data, "SingleCellExperiment")) {
     stop("Data must be a Seurat or SingleCellExperiment object")
   }
@@ -57,33 +45,61 @@ celltypist_majority_vote <- function(data, classification_obs, groups_obs = NULL
   # Determine resolution for clustering if groups_obs is not provided
   if (is.null(groups_obs)) {
     if (is(data, "Seurat")) {
-      resolution <- 5 + 5 * (nrow(data@meta.data) %/% 20000)
-      # Check if the specified graph is available for clustering
       if (!(graph %in% names(data@graphs))) {
-        stop(paste("Provided graph.name", graph, "not present in Seurat object. Please run FindNeighbors() first."))
+        stop(paste("Provided graph name", graph, "not present in Seurat object. Please run FindNeighbors() first."))
       }
-      data <- FindClusters(data, graph.name = graph, resolution = resolution)
-      groups_obs <- paste0("seurat_clusters_", resolution)
+       adj<- as.matrix(data@graphs[[graph]])
+       gr <- graph_from_adjacency_matrix(adj, mode = "undirected", weighted = TRUE)
+       leid<- cluster_leiden(
+              gr,
+              weights = NULL,
+              resolution_parameter = res,
+              beta = 0.01,
+              n_iterations = -1,
+              vertex_weights = NULL
+            )
+      data@meta.data$overclustering <- leid$membership
+      groups_obs<- 'overclustering'
+      obs<- data@meta.data
     } else if (is(data, "SingleCellExperiment")) {
-      resolution <- 5 + 5 * (nrow(colData(data)) %/% 20000)
-      data <- clusterExperiment::ClusterExperiment(data, method = "Leiden", resolution = resolution)
-      groups_obs <- paste0("leiden_", resolution)
+         if (!(graph %in% names(metadata(data)$graphs))) {
+        stop(paste("Provided graph name", graph, "not present in Seurat object. Please run FindNeighbors() first."))
+      }
+   adj<- as.matrix(data@metadata$graphs[[graph]])
+       gr <- graph_from_adjacency_matrix(adj, mode = "undirected", weighted = TRUE)
+       leid<- cluster_leiden(
+              gr,
+              weights = NULL,
+              resolution_parameter = res,
+              beta = 0.01,
+              n_iterations = -1,
+              vertex_weights = NULL
+            )
+      colData(data)$overclustering <- leid$membership
+      groups_obs<- 'overclustering'
+      obs <- colData(data)
     }
-    message(paste("Reference annotation not selected. Computing over-clustering with Leiden algorithm (resolution=", resolution, ") ...", sep = ""))
-    message(paste("Dataset has been divided into", length(unique(obs[[groups_obs]])), "groups according to transcriptional similarities."))
-    message(paste0("Over-clustering result saved in metadata as leiden_", as.character(resolution) ))
-  } else {
-    message(paste(groups_obs, " in metadata has been selected as reference annotation."))
+    message("Reference annotation not selected. Computing over-clustering with Leiden algorithm")
   }
+
+  # Check if groups_obs column exists
+  if (!(groups_obs %in% colnames(obs))) {
+    stop(paste("Column", groups_obs, "not found in metadata. Please check the clustering step."))
+  }
+
+  message(paste("Dataset has been divided into", length(unique(obs[[groups_obs]])), "groups according to transcriptional similarities."))
+  message(paste0("Over-clustering result saved in metadata as ", groups_obs))
 
   message("Extending the more represented cell type label to each cell group...\n")
 
   groups <- obs[[groups_obs]]
 
-  # Ensure classification_obs is a list
-  if (!is.list(classification_obs)) {
-    classification_obs <- list(classification_obs)
+  #Check if classification_obs columns exist
+  missing_columns <- classification_obs[!(classification_obs %in% colnames(obs))]
+  if (length(missing_columns) > 0) {
+    stop(paste("The following classification_obs columns are missing in metadata:", paste(missing_columns, collapse = ", ")))
   }
+
 
   for (classification in classification_obs) {
     votes <- table(obs[[classification]], groups)
@@ -99,7 +115,7 @@ celltypist_majority_vote <- function(data, classification_obs, groups_obs = NULL
   if (is(data, "Seurat")) {
     data@meta.data <- obs
   } else if (is(data, "SingleCellExperiment")) {
-    colData(data) <- DataFrame(obs)
+    colData(data) <- S4Vectors::DataFrame(obs)
   }
 
   return(data)
